a614e18859d32c6b316bb43c3b56fae3790b8e7e
uoneway <uoneway@gmail.com>
2023-11-12 23:33:27 +0900

    Modify MAX_OUTPUT_TOKENS to 1000

--- src/common/consts.py
+++ src/common/consts.py
@@ -16,7 +16,7 @@ TO_JSON = True
 LLM_TEMPERATURE: int = 0
 # {"name": "gpt-4-32k", "max_tokens": 32768}, {"name": "gpt-3.5-turbo-16k", "max_tokens": 16385}]
 MODEL_TYPE_INFOS = [{"name": "gpt-4-1106-preview", "max_tokens": 128000}]
-MAX_OUTPUT_TOKENS = 650
+MAX_OUTPUT_TOKENS = 1000
 OPENAI_RETRIES = 3
 
 # Input
